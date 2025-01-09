0136d03d8d7f3e954d58fb93c53957a6e7ba96e1
Raína <raiina.py@gmail.com>
2025-01-09 18:13:56 -0300

    Update About.jsx

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -26,6 +26,7 @@ const About = () => {
             <p>
                 Please note that this site is not affiliated with or endorsed by NBC or the creators of <em>The Office</em>. All data is sourced from the APIs mentioned above, and all rights belong to their respective owners.
             </p>
+            <p>Developed by Raína Araújo </p>
         </div>
     );
 };
